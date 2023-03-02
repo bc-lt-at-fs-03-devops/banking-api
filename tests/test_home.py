f8563280ffc0f152a129af510ec247dda2f07a0c
DMGochoa <100704377+DMGochoa@users.noreply.github.com>
2023-03-02 14:36:17 -0500

    Update test_home.py
    The prints are eliminated

--- tests/test_home.py
+++ tests/test_home.py
@@ -18,19 +18,16 @@ def test_home(client):
     } 
     # User data to login
     user_creation_response = client.post(f"{USERS_ENDPOINT}",json=user_json)
-    print(user_creation_response.json)
     login_json = {
         "username": user_creation_response.json["username"],
         "password": user_creation_response.json["password"],
         "code": user_creation_response.json["code"] # The type of the "code" is a string and its is supposed to be integer
     }
-    print(login_json)
     login_response = client.post(f"{LOGIN_ENDPOINT}",
                                  json=login_json)
     
-    print(login_response.text)
     home_response = client.get(f'{HOME_ENDPOINT}',  
                                headers={'Authorization':login_response.json['access_token']})
-    print(home_response.status_code)
+
     for key in user_json.keys():
         assert user_json[key] == home_response.json[key]
